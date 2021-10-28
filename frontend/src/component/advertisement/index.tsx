82a819d358b97edc07d9cbdce273282d9f2555df
t.m <norakuro8386@gmail.com>
2021-10-28 21:23:07 +0900

    fix design

--- frontend/src/component/advertisement/index.tsx
+++ frontend/src/component/advertisement/index.tsx
@@ -1,28 +1,21 @@
 import { useLayoutEffect, useRef } from 'react';
 
+import styles from './index.module.scss';
+
 
 export default function Advertisement(props) {
-    const inter = `
-    <script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "8c0a5520b7e5a52529b010c8d7009d44",type: "action"});</script>
-    <script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>`;
+    const inter = `<script src="https://adm.shinobi.jp/s/8c0a5520b7e5a52529b010c8d7009d44"></script>
+    <script src="https://adm.shinobi.jp/s/d3b2d4363aec0a4207a46e8ed51c25d8"></script>`;
 
-    const smartPhonHtml = `<script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "e5bd851fb6048282a5037dbcfdcda547",type: "overlay"});</script>
-    <script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>`;
-
-    const smartPhonAdHtml = `<script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "ad9d235476e7ed671d6480df04a78d78",type: "overlay"});</script>
-    <script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>`;
-
-    const htmlString = `
+    const htmlAdString = `
     <div class="admax-ads" data-admax-id="66ba52cd36f362ffa9a52d2722a62baa" style="display:inline-block;width:160px;height:600px;"></div>
     <script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "66ba52cd36f362ffa9a52d2722a62baa",type: "banner"});</script>
-    <script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>
-    ${inter} ${smartPhonAdHtml}`;
+    <script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>${inter}`;
 
-    const htmlString2 = `
+    const htmlString = `
     <div class="admax-ads" data-admax-id="4ac74c10568cda6e0688eed236b6a8bc" style="display:inline-block;width:160px;height:600px;"></div>
     <script type="text/javascript">(admaxads = window.admaxads || []).push({admax_id: "4ac74c10568cda6e0688eed236b6a8bc",type: "banner"});</script>
-    <script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>
-    ${inter} ${smartPhonAdHtml}`;
+    <script type="text/javascript" charset="utf-8" src="https://adm.shinobi.jp/st/t.js" async></script>${inter}`;
 
     const divRef = useRef();
   
@@ -33,13 +26,17 @@ export default function Advertisement(props) {
 
     const fragment = document
         .createRange()
-        .createContextualFragment(htmlString2);
+        .createContextualFragment(htmlString);
 
         if (divRef.current) {
-        let refCurrent: any = divRef.current;
-        refCurrent.appendChild(fragment);
+            let refCurrent: any = divRef.current;
+            refCurrent.appendChild(fragment);
         }
-    }, [htmlString2]);
-  
-  return <div ref={divRef} />;
+    }, [htmlString]);
+
+  return <>
+            <div className={styles.ad_disp}>
+                <div ref={divRef} />
+            </div>
+        </>;
 }
